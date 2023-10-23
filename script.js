a5d98d9307e589f5d7433cac6d6f317533220f29
benlr98 <benlr98@gmail.com>
2023-10-23 10:38:14 -0600

    handleGuess functionality added

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
 
-elKeyboardDiv = document.getElementById("keyboard")
+// keyboard to push new letter buttons to 
+let elKeyboardDiv = document.getElementById("keyboard");
+let elHiddenWordDiv = document.getElementById("hidden-word");
+let elStrikesSpan = document.getElementById("strikes");
+
+let secretWord = "polar bear";
+// include space so it doesn't have to be guessed 
+let guessedLetters = [" "];
+let strikes = 0;
+
+
+function updateHiddenWord() {
+    let secretWordArray = secretWord.split("")
+    elHiddenWordDiv.innerHTML = "";
+
+    secretWordArray.forEach(letter => {
+        letterDivEl = document.createElement("div");
+        letterDivEl.className = "guess-letter";
+        isGuessedLetter(letter, guessedLetters);
+        letterDivEl.innerText = isGuessedLetter(letter, guessedLetters) ? letter: "";
+        elHiddenWordDiv.appendChild(letterDivEl);
+    })
+}
+
+function updateStrikes() {
+    elStrikesSpan.innerText = strikes;
+}
+
+function isGuessedLetter(letter, guessedLetterArray) {
+    let isGuessed = guessedLetterArray.includes(letter)
+    return isGuessed
+}
 
 function createKeyboard() {
     // create keyboard array of letters 
-    lettersString = "abcdefghijklmnopqrstuvwxyz"
-    lettersArray = lettersString.split("")
-
+    let lettersString = "abcdefghijklmnopqrstuvwxyz"
+    let lettersArray = lettersString.split("")
+    
     lettersArray.forEach(letter => {
-        letterButtonEl = document.createElement("div")
+        let letterButtonEl = document.createElement("div")
         letterButtonEl.className = "key-button"
         letterButtonEl.innerText = letter
         elKeyboardDiv.appendChild(letterButtonEl)
+        letterButtonEl.addEventListener("click", handleGuess)
         
     });
-    
-    
     
 }
 
 
+function handleGuess(e) {
+    let isCorrect = secretWord.includes(e.target.innerText)
+    guessedLetters.push(e.target.innerText)
+    if (!isCorrect) {
+        strikes++
+    }
+    updateHiddenWord()
+    updateStrikes()
+    e.target.removeEventListener("click", handleGuess)
+
+    return isCorrect
+    
+}
+
 // create display and start game 
+updateHiddenWord()
 createKeyboard()
